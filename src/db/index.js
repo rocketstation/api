995624b48e455760a148131d28d3b5066062b59a
oleksiipysanko <oleksiipysanko@rstation.io>
2019-03-19 15:09:45 +0200

    default DB connection string update;

--- src/db/index.js
+++ src/db/index.js
@@ -2,13 +2,19 @@ import Bluebird from 'bluebird'
 import { parse } from 'pg-connection-string'
 
 const parseDBConfig = ({ connectionURL, ...rest }) => {
-  const getResult = ({ database, host, password, port, user, username }) => ({ connectionString: `postgres://${user || username}:${password}@${host}:${port}`, name: database })
+  const getResult = ({ database, host, password, port, user, username }) => {
+    const u = user || username
+
+    return { connectionString: `postgres://${user || username}:${password}@${host}:${port}`, name: database, user: u }
+  }
+
   if (connectionURL) return getResult(parse(connectionURL))
+
   return getResult(rest)
 }
 
 const load = (config) => {
-  const { connectionString, name } = parseDBConfig(config)
+  const { connectionString, name, user } = parseDBConfig(config)
   if (!name) {
     return Bluebird.reject('database name not found. Check your db.js configs')
   }
@@ -20,17 +26,21 @@ const load = (config) => {
     connectionString,
     async create (connection, pgp) {
       const dbExists = await result.checkIfExists(connection)
+
       if (!dbExists) {
         await connection.query(`create database ${name}`)
+
         const dbConnection = result.getConnection(pgp, `${connectionString}/${name}`)
+
         await dbConnection.query('create table if not exists "rstation_api_migrations" ("title" varchar(255) not null unique)')
+
         return dbConnection
       }
     },
     delete (connection) {
       return connection.query(`drop database if exists ${name}`)
     },
-    getConnection (pgp, connectionStr = connectionString) {
+    getConnection (pgp, connectionStr = `${connectionString}/${user}`) {
       return pgp(connectionStr)
     },
     name,
